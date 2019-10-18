5d1e9e5d0e418fef22f5d47ce04a36e605e04add
alexandrafren <alexandra.fren@gmail.com>
2019-10-18 10:20:20 -0700

    extract rectangle formulas into a node module

--- index.js
+++ index.js
@@ -1,20 +1,17 @@
-let rect = {
-    perimeter: (x,y) => (2*(x+y)),
-    area: (x,y) => (x*y)
-    }
+let rect = require('./rectangle');
+
     function solveRect(l,b) {
-        console.log("Solving for rectangle with l = " + l + "and b = " )
+        console.log("Solving for rectangle with l = " + l + " and b = " + b )
         if ( l <= 0 || b <= 0){
-            console.log("Rectangle dimensions should be greater than zero")
+            console.log("Rectangle dimensions should be greater than zero. l = " + l + " and b = " + b)
         }
         else {
-            console.log("The area of the rectange is " + rect.area(l,b))
-            console.log("The permiter of the rectange is " + rect.perimeter(l,b))
+            console.log("The area of the rectangle is " + rect.area(l,b))
+            console.log("The permiter of the rectangle is " + rect.perimeter(l,b))
         }
     }
+
     solveRect(2,4)
     solveRect(3,5)
     solveRect(0,5)
     solveRect(-3,5)
-
-}
